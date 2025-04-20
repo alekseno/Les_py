b2c2a37717ea446430b8dad77300ec7033828256
Alekseno <aleksenocode@gmail.com>
2025-04-20 22:08:19 +0300

    67-69

--- lessons_lesya/LES_STRING/les_67.py
+++ lessons_lesya/LES_STRING/les_67.py
@@ -1,11 +1,13 @@
 import re
-#name = input()
-pattern = "[aeiouy]+"
+name = input()
+replacements = str.maketrans({"a": "97", "A": "65", "e": "101", "E":"69", "i": "105", "I": "73", "o": "111", "O": "79", "u": "117", "U": "85", "y": "121", "Y": "89"})
+res = name.translate(replacements)
+print(res)
 
-a = ord("a")
-A = ord("A")
-e = ord("e")
-E = ord('E')
-#repl = ""
-result = re.sub(pattern, repl, "ahjjeuyytti")
-print(result)
+
+"""Задача на замена гласных букв на их коды из unicoda ("a" = 97)"""
+
+"""s = input()
+for i in 'aeiouyAEIOUY':
+    s = s.replace(i, str(ord(i)))
+print(s)"""
